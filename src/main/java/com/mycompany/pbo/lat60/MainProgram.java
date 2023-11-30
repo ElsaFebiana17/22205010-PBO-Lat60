8e10b79e7be290950fd31a6a4158931e16a48e42
ElsaFebiana17 <145949095+ElsaFebiana17@users.noreply.github.com>
2023-11-30 09:21:10 +0700

    Update MainProgram.java

--- src/main/java/com/mycompany/pbo/lat60/MainProgram.java
+++ src/main/java/com/mycompany/pbo/lat60/MainProgram.java
@@ -7,6 +7,10 @@ package com.mycompany.pbo.lat60;
 /**
  *
  * @author lenovo
+ *Nama : Elsa Febiana
+ *NIM : 22205010
+ *Prodi : Teknik Informatika
+ *Semester : 3
  */
 public class PBOLat60 {
     
